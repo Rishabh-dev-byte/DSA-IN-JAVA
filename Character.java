5e972036da4989757ed37f97954ac8d69929e33d
Rishabh Tiwari <rishabhtiwari3479@gmail.com>
2026-03-08 02:43:39 +0530

    Add implementation for finding the next greatest letter in a character array

---
+++ Character.java
@@ -0,0 +1,13 @@
+class Character {
+    
+    public char nextGreatestLetter(char[] letters, char target) {
+        
+        for(int i=0;i<=letters.length-1;i++){
+            if(letters[i]>target){
+                   return letters[i];
+            }
+           
+        }
+        return letters[0];
+    }
+}
