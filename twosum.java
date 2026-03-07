0e9aca42df345fbb32b33fc10a74d13a1cc60a85
Rishabh Tiwari <rishabhtiwari3479@gmail.com>
2026-03-08 02:10:58 +0530

    Added implementation for two-sum problem

---
+++ twosum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int arr[]=new int[2];
+        
+        for(int i=0;i<=nums.length-2;i++){
+            for(int j=i+1;j<=nums.length-1;j++){
+            int sum = nums[i]+nums[j];
+            if(sum==target){
+                arr = new int[]{i, j};
+            }
+        }
+
+    }
+    return arr;
+    }
+
+}
