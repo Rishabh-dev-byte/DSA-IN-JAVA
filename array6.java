1df3f685a1464e03fdd9eac28cee480c93a158af
Rishabh Tiwari <rishabhtiwari3479@gmail.com>
2026-03-12 01:45:36 +0530

    Add implementation for array rotation by k steps using reversal algorithm

--- array6.java
+++ array6.java
@@ -11,7 +11,7 @@ class Solution {
     }
 }
 
-public class array5{
+public class array6{
     public static void main(String[] args){
         System.out.println("enter the size of the array");
         Scanner obj = new Scanner(System.in);
@@ -22,37 +22,13 @@ public class array5{
         }
         System.out.println("enter the place by the array is to be rotated");
         int k = obj.nextInt();
-         Solution.reverse(arr,0,k-1);
-         Solution.reverse(arr,k,arr.length-1);
+         Solution.reverse(arr,0,arr.length-k-1);
+         Solution.reverse(arr,arr.length-k,arr.length-1);
          Solution.reverse(arr,0,arr.length-1);
          for(int i=0;i<=arr.length-1;i++){
             System.out.print(arr[i]+"");
         }
         obj.close();
     
-    }
-}
-
-// if k is greater than array size
-class Solution {
-
-    public void rotate(int[] nums, int k) {
-
-         k = k % nums.length; 
-
-        reverse(nums, 0, nums.length-k-1);
-        reverse(nums, nums.length-k, nums.length-1);
-        reverse(nums, 0, nums.length - 1);
-    }
-
-    public void reverse(int[] arr, int start, int end) {
-        
-        while (start < end) {
-            int temp = arr[end];
-            arr[end] = arr[start];
-            arr[start] = temp;
-            start++;
-            end--;
-        }
     }
 }
