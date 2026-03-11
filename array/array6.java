38e54c0cb03594dc1df23c96f5ad7eee62dd55ad
Rishabh Tiwari <rishabhtiwari3479@gmail.com>
2026-03-12 01:51:25 +0530

    Add rotate method to Solution class for array rotation by k steps

--- array/array6.java
+++ array/array6.java
@@ -33,3 +33,26 @@ public class array6{
     
     }
 }
+// if k is greater than array size
+class Solution {
+
+    public void rotate(int[] nums, int k) {
+
+         k = k % nums.length; 
+
+        reverse(nums, 0, nums.length-k-1);
+        reverse(nums, nums.length-k, nums.length-1);
+        reverse(nums, 0, nums.length - 1);
+    }
+
+    public void reverse(int[] arr, int start, int end) {
+        
+        while (start < end) {
+            int temp = arr[end];
+            arr[end] = arr[start];
+            arr[start] = temp;
+            start++;
+            end--;
+        }
+    }
+}
