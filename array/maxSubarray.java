4ae95f891bc08097eef83275a7fc3dafdb43b6b1
Rishabh Tiwari <rishabhtiwari3479@gmail.com>
2026-03-18 02:33:54 +0530

    Refactor maxSubArray method for improved readability and maintainability LC-53

--- array/maxSubarray.java
+++ array/maxSubarray.java
@@ -1,18 +1,54 @@
+// to print the max of the subarray
 class maxSubArray {
     public int maxSubArray(int[] nums) {
-        int sum=0;
+        int sum = 0;
         int max = -100000;
-       for(int i=0;i<nums.length;i++){
-                sum += nums[i];
 
-            if(sum>max){
-                max=sum;
+        int newstart;
+        int start;
+        int end;
+        for (int i = 0; i < nums.length; i++) {
+
+            sum += nums[i];
+
+            if (sum > max) {
+                max = sum;
+
             }
 
-            if(sum<0){
-                sum=0;
+            if (sum < 0) {
+                sum = 0;
             }
-       }
-       return max;
+        }
+        return max;
     }
 }
+
+// to print the max sub array
+
+// class maxSubArray {
+// public int maxSubArray(int[] nums) {
+// int sum=0;
+// int max = -100000;
+
+// int newstart;
+// int start;
+// int end;
+// for(int i=0;i<nums.length;i++){
+// if(sum==0) start=i;
+// sum += nums[i];
+
+// if(sum>max){
+// max=sum;
+// newstart=start;
+// end=i;
+
+// }
+
+// if(sum<0){
+// sum=0;
+// }
+// }
+// return max;
+// }
+// }
