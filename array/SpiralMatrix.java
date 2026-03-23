7338f9b35687f2866b4cefa36bd126063ecca446
Rishabh Tiwari <rishabhtiwari3479@gmail.com>
2026-03-24 01:08:07 +0530

    LC-54 spiral matrix

---
+++ array/SpiralMatrix.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    
+    public List<Integer> spiralOrder(int[][] matrix) {
+       List<Integer> spiralOrder = new ArrayList<>();
+       int top=0;
+       int bottom=matrix.length-1;
+       int left=0;
+       int right=matrix[top].length-1;
+
+       while(top<=bottom && left<=right){
+        for(int i=left;i<=right;i++){
+            spiralOrder.add(matrix[top][i]);
+        }
+        top++;
+        for(int i=top;i<=bottom;i++){
+            spiralOrder.add(matrix[i][right]);
+        }
+        right--;
+        if(top<=bottom){
+            for(int i=right;i>=left;i--){
+                spiralOrder.add(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        if(left<=right){
+            for(int i=bottom;i>=top;i--){
+                spiralOrder.add(matrix[i][left]);
+            }
+        left++;
+        }
+       }
+       return spiralOrder;
+    }
+}
+
